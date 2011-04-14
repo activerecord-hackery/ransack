7264896d431bdbd9c3e7e6d72d2453ac1fa65be1
Ernie Miller <ernie@metautonomo.us>
2011-04-14 19:16:40 -0400

    Thanks, @jonleighton!

--- lib/ransack/adapters/active_record/join_association.rb
+++ lib/ransack/adapters/active_record/join_association.rb
@@ -3,7 +3,6 @@ require 'active_record'
 module Ransack
   module Adapters
     module ActiveRecord
-
       class JoinAssociation < ::ActiveRecord::Associations::JoinDependency::JoinAssociation
 
         def initialize(reflection, join_dependency, parent = nil, polymorphic_class = nil)
@@ -29,68 +28,17 @@ module Ransack
           super && active_record == other.active_record
         end
 
-        # This is a temporary hack. Jon's going to refactor this in AR to
-        # make overrides simpler
-        def join_to(relation)
-          tables        = @tables.dup
-          foreign_table = parent_table
-
-          # The chain starts with the target table, but we want to end with it here (makes
-          # more sense in this context), so we reverse
-          chain.reverse.each_with_index do |reflection, i|
-            table = tables.shift
-
-            case reflection.source_macro
-            when :belongs_to
-              key         = reflection.association_primary_key
-              foreign_key = reflection.foreign_key
-            when :has_and_belongs_to_many
-              # Join the join table first...
-              relation.from(join(
-                table,
-                table[reflection.foreign_key].
-                  eq(foreign_table[reflection.active_record_primary_key])
-              ))
-
-              foreign_table, table = table, tables.shift
-
-              key         = reflection.association_primary_key
-              foreign_key = reflection.association_foreign_key
-            else
-              key         = reflection.foreign_key
-              foreign_key = reflection.active_record_primary_key
-            end
-
-            constraint = table[key].eq(foreign_table[foreign_key])
-
-            if reflection.options[:polymorphic]
-              constraint = constraint.and(
-                foreign_table[reflection.foreign_type].eq(reflection.klass.name)
-              )
-            end
-
-            if reflection.klass.finder_needs_type_condition?
-              constraint = table.create_and([
-                constraint,
-                reflection.klass.send(:type_condition, table)
-              ])
-            end
-
-            relation.from(join(table, constraint))
-
-            unless conditions[i].empty?
-              relation.where(sanitize(conditions[i], table))
-            end
-
-            # The current table in this iteration becomes the foreign table in the next
-            foreign_table = table
+        def build_constraint(reflection, table, key, foreign_table, foreign_key)
+          if reflection.options[:polymorphic]
+            super.and(
+              foreign_table[reflection.foreign_type].eq(reflection.klass.name)
+            )
+          else
+            super
           end
-
-          relation
         end
 
       end
-
     end
   end
 end
