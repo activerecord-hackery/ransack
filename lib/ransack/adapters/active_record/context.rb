8c352fc580209b66d0678ad62c91eea470fea446
Yury Velikanau <yury.velikanau@gmail.com>
2013-02-13 20:55:15 -0800

    * Add rails 3.2 context and default context for edge rails * Add relation_for method to return relation correctly (rails 3.0-3.2 wants #scoped, edge rails wants #all)

--- lib/ransack/adapters/active_record/context.rb
+++ lib/ransack/adapters/active_record/context.rb
@@ -1,5 +1,5 @@
 require 'ransack/context'
-require 'ransack/adapters/active_record/3.1/context'
+require 'ransack/adapters/active_record/3.2/context'
 require 'ransack/adapters/active_record/compat'
 require 'polyamorous'
 
@@ -8,30 +8,13 @@ module Ransack
     module ActiveRecord
       class Context < ::Ransack::Context
 
-        # Redefine a few things that have changed with 3.2.
-
         def initialize(object, options = {})
           super
           @arel_visitor = @engine.connection.visitor
         end
 
-        def type_for(attr)
-          return nil unless attr && attr.valid?
-          name    = attr.arel_attribute.name.to_s
-          table   = attr.arel_attribute.relation.table_name
-
-          schema_cache = @engine.connection.schema_cache    
-          raise "No table named #{table} exists" unless schema_cache.table_exists?(table)  
-          schema_cache.columns_hash[table][name].type
-        end
-
-        def evaluate(search, opts = {})
-          viz = Visitor.new
-          relation = @object.where(viz.accept(search.base))
-          if search.sorts.any?
-            relation = relation.except(:order).order(viz.accept(search.sorts))
-          end
-          opts[:distinct] ? relation.uniq : relation
+        def relation_for(object)
+          object.all
         end
 
       end
