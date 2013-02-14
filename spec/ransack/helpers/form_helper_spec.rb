f4408e0ac93721d723f7320b172946705599dfc3
Yury Velikanau <yury.velikanau@gmail.com>
2013-02-14 11:48:09 -0800

    Ruby 1.8 hash syntax, fixes builds on ruby18, ree, rbx-18mode

--- spec/ransack/helpers/form_helper_spec.rb
+++ spec/ransack/helpers/form_helper_spec.rb
@@ -7,7 +7,7 @@ module Ransack
       router = ActionDispatch::Routing::RouteSet.new
       router.draw do
         resources :people
-        match ':controller(/:action(/:id(.:format)))', via: :get
+        match ':controller(/:action(/:id(.:format)))', :via => :get
       end
 
       include router.url_helpers
