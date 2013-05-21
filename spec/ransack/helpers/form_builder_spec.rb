535a7787e147ad861de5d8cfad78c98c01a4ba9e
Yury Velikanau <yury.velikanau@gmail.com>
2013-05-20 17:49:37 -0700

    Fix incorrectly resolved merge.

--- spec/ransack/helpers/form_builder_spec.rb
+++ spec/ransack/helpers/form_builder_spec.rb
@@ -50,14 +50,10 @@ module Ransack
       describe '#sort_link' do
         it 'sort_link for ransack attribute' do
           sort_link = @f.sort_link :name, :controller => 'people'
-          sort_link.should match /people\?q%5Bs%5D=name\+asc/
+          sort_link.should match /people\?q(%5B|\[)s(%5D|\])=name\+asc/
           sort_link.should match /sort_link/
           sort_link.should match /Full Name<\/a>/
         end
-
-        it { should match /people\?q(%5B|\[)s(%5D|\])=name\+asc/}
-        it { should match /sort_link/}
-        it { should match /Full Name<\/a>/}
 
         it 'sort_link for common attribute' do
           sort_link = @f.sort_link :id, :controller => 'people'
