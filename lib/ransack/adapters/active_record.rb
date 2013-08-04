8db7a3c1f4242305488a541267dd87c5397f5707
jonatack <jonnyatack@gmail.com>
2013-08-04 18:40:18 +0200

    Remove Rails 3 active_record code to optimize for Rails 4

--- lib/ransack/adapters/active_record.rb
+++ lib/ransack/adapters/active_record.rb
@@ -2,13 +2,4 @@ require 'active_record'
 require 'ransack/adapters/active_record/base'
 ActiveRecord::Base.extend Ransack::Adapters::ActiveRecord::Base
 
-case ActiveRecord::VERSION::STRING
-when /^3\.0\./
-  require 'ransack/adapters/active_record/3.0/context'
-when /^3\.1\./
-  require 'ransack/adapters/active_record/3.1/context'
-when /^3\.2\./
-  require 'ransack/adapters/active_record/3.2/context'
-else
-  require 'ransack/adapters/active_record/context'
-end
+require 'ransack/adapters/active_record/context'
