69ee4da6e12fc3060a39c5fecd730fc67976881b
jonatack <jonnyatack@gmail.com>
2013-08-16 21:04:37 +0200

    No need for nil checking in commit #7597f0d

--- lib/ransack/search.rb
+++ lib/ransack/search.rb
@@ -14,12 +14,10 @@ module Ransack
              :translate, to: :base
 
     def initialize(object, params = {}, options = {})
-      params ||= {}
+      (params ||= {}).delete_if { |k, v| v.blank? && v != false }
       @context = Context.for(object, options)
       @context.auth_object = options[:auth_object]
       @base = Nodes::Grouping.new(@context, 'and')
-      params = (params.delete_if { |k, v| v.blank? && v != false }
-        ) if params.present?
       build(params.with_indifferent_access)
     end
 
