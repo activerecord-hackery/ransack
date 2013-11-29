37dbebc579cbf797f6715109770e09099b1f5599
Denis Tataurov <dtataurov@at-consulting.ru>
2013-11-29 18:44:51 +0400

    Fix issue where trying to add a polymorphic field in search form resulted in NameError
    Fixes #159
    Fixes #313

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,12 +11,13 @@ Dir[File.expand_path('../{helpers,support,blueprints}/*.rb', __FILE__)].each do 
 end
 
 Sham.define do
-  name     { Faker::Name.name }
-  title    { Faker::Lorem.sentence }
-  body     { Faker::Lorem.paragraph }
-  salary   {|index| 30000 + (index * 1000)}
-  tag_name { Faker::Lorem.words(3).join(' ') }
-  note     { Faker::Lorem.words(7).join(' ') }
+  name       { Faker::Name.name }
+  title      { Faker::Lorem.sentence }
+  body       { Faker::Lorem.paragraph }
+  salary     {|index| 30000 + (index * 1000)}
+  tag_name   { Faker::Lorem.words(3).join(' ') }
+  note       { Faker::Lorem.words(7).join(' ') }
+  notable_id { |id| id }
 end
 
 RSpec.configure do |config|
