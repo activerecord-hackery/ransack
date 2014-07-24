b725ca24c41a050cfcde9853b9de984e413640f7
Jon Atack <jonnyatack@gmail.com>
2014-07-24 16:46:50 +0200

    Fix Rspec deprecation warnings when running test suite

--- spec/ransack/adapters/active_record/base_spec.rb
+++ spec/ransack/adapters/active_record/base_spec.rb
@@ -85,19 +85,19 @@ module Ransack
 
           it "should function correctly when using fields with dots in them" do
             s = Person.search(:email_cont => "example.com")
-            expect(s.result.exists?).to be_true
+            expect(s.result.exists?).to be true
           end
 
           it "should function correctly when using fields with % in them" do
             Person.create!(:name => "110%-er")
             s = Person.search(:name_cont => "10%")
-            expect(s.result.exists?).to be_true
+            expect(s.result.exists?).to be true
           end
 
           it "should function correctly when using fields with backslashes in them" do
             Person.create!(:name => "\\WINNER\\")
             s = Person.search(:name_cont => "\\WINNER\\")
-            expect(s.result.exists?).to be_true
+            expect(s.result.exists?).to be true
           end
 
           it 'allows sort by "only_sort" field' do
