83ac5c9355b4044e95d4bd7399e1c10ebf8a4c2e
Mark Edmondson <mark@guestfolio.com>
2014-08-01 16:31:59 -0700

    Allow flexibility of "or" grouping on base search

--- lib/ransack/search.rb
+++ lib/ransack/search.rb
@@ -19,7 +19,7 @@ module Ransack
       .delete_if { |k, v| [*v].all? { |i| i.blank? && i != false } }
       @context = Context.for(object, options)
       @context.auth_object = options[:auth_object]
-      @base = Nodes::Grouping.new(@context, 'and')
+      @base = Nodes::Grouping.new(@context, options[:grouping] || 'and')
       @scope_args = {}
       build(params.with_indifferent_access)
     end
