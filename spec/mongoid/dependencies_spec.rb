b81bf44dc1b66497969f1fa01995d5e8952367c3
Zhomart Mukhamejanov <mzhomart@gmail.com>
2014-10-06 14:21:22 -0700

    previous fix fixed correctly now

--- spec/mongoid/dependencies_spec.rb
+++ spec/mongoid/dependencies_spec.rb
@@ -1,4 +1,4 @@
-unless ::Rails::VERSION::STRING >= '4'
+unless ::ActiveSupport::VERSION::STRING >= '4'
   describe 'Ransack' do
     it 'can be required without errors' do
       output = `bundle exec ruby -e "require 'ransack'" 2>&1`
