d6e80f2e8883391bcb87fd94f053f1a7a4497ab0
Joe Yates <joe.g.yates@gmail.com>
2014-10-17 14:02:16 +0200

    Check exact matches before splitting
    * if the supplied name contains '_and_' or '_or_' we should check
      if it exists before splitting and checking the parts, this way we
      handle 'terms_and_conditions' as well as pairs of fields like
      'name_and_email'.

--- spec/ransack/nodes/grouping_spec.rb
+++ spec/ransack/nodes/grouping_spec.rb
@@ -7,7 +7,29 @@ module Ransack
         @g = 1
       end
 
+      let(:context) { Context.for(Person) }
 
+      subject { described_class.new(context) }
+
+      describe '#attribute_method?' do
+        context 'for attributes of the context' do
+          it 'is true' do
+            expect(subject.attribute_method?('name')).to be_true
+          end
+
+          context "where the attribute contains '_and_'" do
+            it 'is true' do
+              expect(subject.attribute_method?('terms_and_conditions')).to be_true
+            end
+          end
+        end
+
+        context 'for unknown attributes' do
+          it 'is false' do
+            expect(subject.attribute_method?('not_an_attribute')).to be_false
+          end
+        end
+      end
     end
   end
 end
