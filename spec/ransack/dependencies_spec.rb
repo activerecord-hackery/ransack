15b0dd1c6a09e0207d40332138dbde5bad11e586
jonatack <jonnyatack@gmail.com>
2015-01-08 11:01:09 +0100

    Stop testing Rails 3.0 with Ruby 2.2.
    This will slim down the now-extensive travis
    suite matrix and speed it up.
    
    The next tests to trim out will be Rails 3.1
    with Ruby 2.2.

--- spec/ransack/dependencies_spec.rb
+++ spec/ransack/dependencies_spec.rb
@@ -1,12 +1,10 @@
-ruby, rails  = RUBY_VERSION, ::ActiveRecord::VERSION::STRING.first(3)
+rails = ::ActiveRecord::VERSION::STRING.first(3)
 
-if %w(3.2 4.0 4.1).include?(rails) || (%w(3.0 3.1).include?(rails) && ruby < '2.2')
-
+if %w(3.2 4.0 4.1).include?(rails) || rails == '3.1' && RUBY_VERSION < '2.2'
   describe 'Ransack' do
     it 'can be required without errors' do
       output = `bundle exec ruby -e "require 'ransack'" 2>&1`
       expect(output).to be_empty
     end
   end
-
 end
