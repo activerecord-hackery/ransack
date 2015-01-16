3add178bd26a73e0e18c7bed291c2ccccab08500
jonatack <jonnyatack@gmail.com>
2015-01-16 11:40:40 +0100

    Refactor Nodes::Condition

--- lib/ransack/adapters/active_record/ransack/nodes/condition.rb
+++ lib/ransack/adapters/active_record/ransack/nodes/condition.rb
@@ -3,43 +3,56 @@ module Ransack
     class Condition
 
       def arel_predicate
-        predicates = attributes.map do |attr|
-          attr.attr.send(
-            arel_predicate_for_attribute(attr),
-            formatted_values_for_attribute(attr)
-          )
-        end
-
-        if predicates.size > 1
-          case combinator
-          when Constants::AND
-            Arel::Nodes::Grouping.new(Arel::Nodes::And.new(predicates))
-          when Constants::OR
-            predicates.inject(&:or)
-          end
-        else
-          return_predicate(predicates.first)
-        end
+        arel_predicate_for(attributes_array)
       end
 
       private
 
-        # TODO: Improve this edge case patch for Arel >= 6.0 (Rails >= 4.2)
-        #        that adds several conditionals to handle changing Arel API.
-        #        Related to Ransack issue #472 and pull requests #486-488.
-        #
-        def return_predicate(predicate)
-          if casted_array_with_in_predicate?(predicate)
-            predicate.right[0] = predicate.right[0].val
-            .map { |v| v.is_a?(String) ? Arel::Nodes.build_quoted(v) : v }
+        def attributes_array
+          attributes.map do |a|
+            a.attr.send(
+              arel_predicate_for_attribute(a), formatted_values_for_attribute(a)
+            )
           end
-          predicate
         end
-        #
+
+        def arel_predicate_for(predicates)
+          if predicates.size > 1
+            combinator_for(predicates)
+          else
+            format_predicate(predicates.first)
+          end
+        end
+
+        def combinator_for(predicates)
+          if combinator === Constants::AND
+            Arel::Nodes::Grouping.new(Arel::Nodes::And.new(predicates))
+          elsif combinator === Constants::OR
+            predicates.inject(&:or)
+          end
+        end
+
+        def format_predicate(predicate)
+          predicate.tap do
+            if casted_array_with_in_predicate?(predicate)
+              predicate.right[0] = predicate.right[0].val.map { |v| format(v) }
+            end
+          end
+        end
+
         def casted_array_with_in_predicate?(predicate)
           return unless defined?(Arel::Nodes::Casted)
           predicate.class == Arel::Nodes::In &&
           predicate.right[0].val.is_a?(Array)
+        end
+
+        # Necessary for Arel >= 6.0 (Rails >= 4.2)
+        def format(value)
+          if value.is_a?(String)
+            Arel::Nodes.build_quoted(value)
+          else
+            value
+          end
         end
 
     end
