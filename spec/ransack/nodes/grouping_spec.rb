ba92a0bc3590ecab8f82501586a09d7deee264b8
Jon Atack <jonnyatack@gmail.com>
2015-04-10 22:40:59 +0530

    Replace deprecated RSpec syntax

--- spec/ransack/nodes/grouping_spec.rb
+++ spec/ransack/nodes/grouping_spec.rb
@@ -15,37 +15,37 @@ module Ransack
       describe '#attribute_method?' do
         context 'for attributes of the context' do
           it 'is true' do
-            expect(subject.attribute_method?('name')).to be_true
+            expect(subject.attribute_method?('name')).to be true
           end
 
           context "when the attribute contains '_and_'" do
             it 'is true' do
-              expect(subject.attribute_method?('terms_and_conditions')).to be_true
+              expect(subject.attribute_method?('terms_and_conditions')).to be true
             end
           end
 
           context "when the attribute contains '_or_'" do
             it 'is true' do
-              expect(subject.attribute_method?('true_or_false')).to be_true
+              expect(subject.attribute_method?('true_or_false')).to be true
             end
           end
 
           context "when the attribute ends with '_start'" do
             it 'is true' do
-              expect(subject.attribute_method?('life_start')).to be_true
+              expect(subject.attribute_method?('life_start')).to be true
             end
           end
 
           context "when the attribute ends with '_end'" do
             it 'is true' do
-              expect(subject.attribute_method?('stop_end')).to be_true
+              expect(subject.attribute_method?('stop_end')).to be true
             end
           end
         end
 
         context 'for unknown attributes' do
           it 'is false' do
-            expect(subject.attribute_method?('not_an_attribute')).to be_false
+            expect(subject.attribute_method?('not_an_attribute')).to be false
           end
         end
       end
