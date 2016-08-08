activerecord-hackery/ransack

commit 235eae3b825e16a6a02e37423b2d24f6cda97da5

Refactor: condition handles binding of its attribute

lib/ransack/nodes/condition.rb
@@ -79,14 +79,12 @@ module Ransack
       def attributes=(args)
         case args
         when Array
-          args.each do |attr|
-            attr = Attribute.new(@context, attr)
-            self.attributes << attr if attr.valid?
+          args.each do |name|
+            build_attribute(name)
           end
         when Hash
           args.each do |index, attrs|
-            attr = Attribute.new(@context, attrs[:name], attrs[:ransacker_args])
-            self.attributes << attr if attr.valid?
+            build_attribute(attrs[:name], attrs[:ransacker_args])
           end
         else
           raise ArgumentError,
@@ -129,9 +127,10 @@ module Ransack
       alias :m= :combinator=
       alias :m :combinator
 
-      def build_attribute(name = nil)
-        Attribute.new(@context, name).tap do |attribute|
-          self.attributes << attribute
+      def build_attribute(name = nil, ransacker_args = [])
+        Attribute.new(@context, name, ransacker_args).tap do |attribute|
+          @context.bind(attribute, attribute.name)
+          self.attributes << attribute if attribute.valid?
         end
       end
 

lib/ransack/adapters/active_record/ransack/context.rb
@@ -40,7 +40,7 @@ module Ransack
         @base.table_name, as: @base.aliased_table_name, type_caster: self
         )
       @bind_pairs = Hash.new do |hash, key|
-        parent, attr_name = get_parent_and_attribute_name(key.to_s)
+        parent, attr_name = get_parent_and_attribute_name(key)
         if parent && attr_name
           hash[key] = [parent, attr_name]
         end

lib/ransack/context.rb
@@ -58,6 +58,7 @@ module Ransack
     end
 
     def bind(object, str)
+      return nil unless str
       object.parent, object.attr_name = @bind_pairs[str]
     end
 

lib/ransack/nodes/attribute.rb
@@ -16,7 +16,6 @@ module Ransack
 
       def name=(name)
         @name = name
-        context.bind(self, name) unless name.blank?
       end
 
       def valid?

lib/ransack/nodes/sort.rb
@@ -32,7 +32,7 @@ module Ransack
 
       def name=(name)
         @name = name
-        context.bind(self, name) unless name.blank?
+        context.bind(self, name)
       end
 
       def dir=(dir)

commit 2839acf731f8c0fc3fb25771650df0b70ff133a5

Fix regression introduced with 235eae3. Closes #701.
See the comments in the code for more information.

TODO: Add test coverage for this.

lib/ransack/nodes/condition.rb
@@ -127,10 +127,29 @@ module Ransack
       alias :m= :combinator=
       alias :m :combinator
 
+
+      # == build_attribute
+      #
+      #  This method was originally called from Nodes::Grouping#new_condition
+      #  only, without arguments, without #valid? checking, to build a new
+      #  grouping condition.
+      #
+      #  After refactoring in 235eae3, it is now called from 2 places:
+      #
+      #  1. Nodes::Condition#attributes=, with +name+ argument passed or +name+
+      #     and +ransacker_args+. Attributes are included only if #valid?.
+      #
+      #  2. Nodes::Grouping#new_condition without arguments. In this case, the
+      #     #valid? conditional needs to be bypassed, otherwise nothing is
+      #     built. The `name.nil?` conditional below currently does this.
+      #
+      #  TODO: Add test coverage for this behavior and ensure that `name.nil?`
+      #  isn't fixing issue #702 by introducing untested regressions.
+      #
       def build_attribute(name = nil, ransacker_args = [])
         Attribute.new(@context, name, ransacker_args).tap do |attribute|
           @context.bind(attribute, attribute.name)
-          self.attributes << attribute if attribute.valid?
+          self.attributes << attribute if name.nil? || attribute.valid?
           if predicate && !negative?
             @context.lock_association(attribute.parent)
           end