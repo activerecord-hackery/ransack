368945bede2de38f034b196c675b5cab093cf01b
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-09-03 08:10:55 +0900

    use `ActiveSupport.on_load` to hook into Active Record
    For avoiding autoloading these constants too soon.
    This affect the initialization of Active Record.
    Ref: https://github.com/rails/rails/issues/23589#issuecomment-229247727

--- lib/ransack/adapters/active_record.rb
+++ lib/ransack/adapters/active_record.rb
@@ -1,5 +1,15 @@
 require 'ransack/adapters/active_record/base'
-ActiveRecord::Base.extend Ransack::Adapters::ActiveRecord::Base
+
+ActiveSupport.on_load(:active_record) do
+  extend Ransack::Adapters::ActiveRecord::Base
+
+  Ransack::SUPPORTS_ATTRIBUTE_ALIAS =
+  begin
+    ActiveRecord::Base.respond_to?(:attribute_aliases)
+  rescue NameError
+    false
+  end
+end
 
 require 'ransack/adapters/active_record/context'
 
