7781dfa611ea11afa49bbe09c88e527dc4cdb7c9
Li Dong <dongli@lasg.iap.ac.cn>
2016-10-10 13:27:04 +0800

    Fix a bug
    In some cases, groupings is not a member method of object.

--- lib/ransack/helpers/form_builder.rb
+++ lib/ransack/helpers/form_builder.rb
@@ -10,7 +10,7 @@ module ActionView::Helpers::Tags
       begin
         object.send @method_name if object # use send instead of public_send
       rescue
-        object.groupings.each do |grouping|
+        (object.groupings rescue []).each do |grouping|
           if grouping.tag == @method_name
             # match polymorphic search field
             return grouping.conditions.first.value
