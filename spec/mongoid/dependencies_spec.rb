24e0f1c974fe26caffed8147c300d9672e3172f6
Greg Molnar <molnargerg@gmail.com>
2018-06-25 20:50:14 +0200

    move mongoid support to a separate gem

--- spec/mongoid/dependencies_spec.rb
+++
@@ -1,8 +0,0 @@
-unless ::ActiveSupport::VERSION::STRING >= '4'
-  describe 'Ransack' do
-    it 'can be required without errors' do
-      output = `bundle exec ruby -e "require 'ransack'" 2>&1`
-      expect(output).to be_empty
-    end
-  end
-end
