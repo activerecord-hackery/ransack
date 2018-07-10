46043c293512bd7446be6ae17342b756d6754673
keisuke2236 <terada@basicinc.jp>
2018-07-10 16:21:56 +0900

    mongoid_like

--- lib/ransack/adapters/active_record.rb
+++ lib/ransack/adapters/active_record.rb
@@ -3,11 +3,15 @@ require 'ransack/adapters/active_record/base'
 ActiveSupport.on_load(:active_record) do
   extend Ransack::Adapters::ActiveRecord::Base
 
-  Ransack::SUPPORTS_ATTRIBUTE_ALIAS =
-  begin
-    ActiveRecord::Base.respond_to?(:attribute_aliases)
-  rescue NameError
-    false
+  if defined?(::Mongoid)
+    SUPPORTS_ATTRIBUTE_ALIAS = false
+  else
+    Ransack::SUPPORTS_ATTRIBUTE_ALIAS =
+    begin
+      ActiveRecord::Base.respond_to?(:attribute_aliases)
+    rescue NameError
+      false
+    end
   end
 end
 
