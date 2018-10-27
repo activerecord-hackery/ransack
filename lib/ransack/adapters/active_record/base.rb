0f4be918bf0157a89294d95f6c8dc0065e594d5a
Greg Molnar <molnargerg@gmail.com>
2018-10-27 07:40:53 +0200

    deprecate #search

--- lib/ransack/adapters/active_record/base.rb
+++ lib/ransack/adapters/active_record/base.rb
@@ -14,6 +14,7 @@ module Ransack
         end
 
         def ransack(params = {}, options = {})
+          ActiveSupport::Deprecation.warn("#search is deprecated and will be removed in 2.3, please use #ransack instead") if __callee__ == :search
           Search.new(self, params, options)
         end
 
