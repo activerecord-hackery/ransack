0475c23afd169e2652449b8e463cebaaf343eda2
Timo Schilling <timo@schilling.io>
2018-12-05 10:11:27 +0100

    fix a problem with rails > 5.2.1.1

--- lib/polyamorous.rb
+++ lib/polyamorous.rb
@@ -13,7 +13,7 @@ if defined?(::ActiveRecord)
 
   ar_version = ::ActiveRecord::VERSION::STRING[0,3]
   ar_version = ::ActiveRecord::VERSION::STRING[0,5] if ar_version >= "5.2"
-  ar_version = "5.2.1" if ::ActiveRecord::VERSION::STRING >= "5.2.1"
+  ar_version = "5.2.1" if ::ActiveRecord::VERSION::STRING == "5.2.1.1"
 
   %w(join_association join_dependency).each do |file|
     require "polyamorous/activerecord_#{ar_version}_ruby_2/#{file}"
