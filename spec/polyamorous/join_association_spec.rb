590efd79983bc374e8bf5c7206791645459078e0
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2021-06-19 10:51:17 +0900

    fix typo

--- spec/polyamorous/join_association_spec.rb
+++ spec/polyamorous/join_association_spec.rb
@@ -17,7 +17,7 @@ module Polyamorous
       expect(subject).not_to eq new_join_association(reflection, parent.children, Article)
     end
 
-    it 'leaves the orginal reflection intact for thread safety' do
+    it 'leaves the original reflection intact for thread safety' do
       reflection.instance_variable_set(:@klass, Article)
       join_association
       .swapping_reflection_klass(reflection, Person) do |new_reflection|
