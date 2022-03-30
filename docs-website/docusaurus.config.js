8bf56492183d9e9235102eca59cf9fe06b0021a0
David Rodríguez <deivid.rodriguez@riseup.net>
2022-03-30 14:44:04 +0200

    Show docs by default in homepage

--- docs-website/docusaurus.config.js
+++ docs-website/docusaurus.config.js
@@ -22,6 +22,7 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           editUrl: 'https://github.com/activerecord-hackery/ransack/docs-website/docs/',
         },
