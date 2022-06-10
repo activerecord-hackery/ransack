7af41376a61b98b81512ccdc54c5ff9f847dcaa8
Junichi Ito <jit@sonicgarden.jp>
2022-06-10 18:36:59 +0900

    Ignore ActiveModel::RangeError in Ransack conditions Fixes https://github.com/activerecord-hackery/ransack/issues/1337

--- lib/ransack/adapters/active_record/ransack/nodes/condition.rb
+++ lib/ransack/adapters/active_record/ransack/nodes/condition.rb
@@ -3,7 +3,7 @@ module Ransack
     class Condition
 
       def arel_predicate
-        attributes.map { |attribute|
+        predicate = attributes.map { |attribute|
           association = attribute.parent
           if negative? && attribute.associated_collection?
             query = context.build_correlated_subquery(association)
@@ -19,6 +19,17 @@ module Ransack
             format_predicate(attribute)
           end
         }.reduce(combinator_method)
+
+        if replace_right_node?(predicate)
+          # Replace right node object to plain integer value in order to avoid
+          # ActiveModel::RangeError from Arel::Node::Casted.
+          # The error can be ignored here because RDBMSs accept large numbers
+          # in condition clauses.
+          plain_value = predicate.right.value
+          predicate.right = plain_value
+        end
+
+        predicate
       end
 
       private
@@ -54,6 +65,17 @@ module Ransack
           predicate.value.map do |val|
             val.is_a?(String) ? Arel::Nodes.build_quoted(val) : val
           end
+        end
+
+        def replace_right_node?(predicate)
+          return false unless predicate.is_a?(Arel::Nodes::Binary)
+
+          arel_node = predicate.right
+          return false unless arel_node.is_a?(Arel::Nodes::Casted)
+
+          relation, name = arel_node.attribute.values
+          attribute_type = relation.type_for_attribute(name).type
+          attribute_type == :integer && arel_node.value.is_a?(Integer)
         end
 
     end
